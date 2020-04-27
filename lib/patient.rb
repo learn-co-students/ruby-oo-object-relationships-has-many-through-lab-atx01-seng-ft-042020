dd22ea970b5ae4610c43b1f57034ba6b97868c66
rhinehardtc <rhinehardtc@gmail.com>
2020-04-26 20:43:38 -0500

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,25 @@
+class Patient
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select {|apt| apt.patient == self}
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+
+    def doctors
+        appointments.map {|apt| apt.doctor}
+    end
+end
